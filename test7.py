0c836ca5b6c5ff789f91ea8e1a2db0f467ba313e
TA-PHIL <1392481118@qq.com>
2023-05-16 21:02:08 +0800

    2023年05月16号，gmd和pd的时间对准问题基本解决 以test8.py为准，其是test5.py的改进版本， 主要针对gmd的数据修正，但是没有主动修正pd的数据

--- test7.py
+++ test7.py
@@ -7,9 +7,6 @@ import matplotlib.pyplot as plt
 # 读取CSV文件
 df = pd.read_csv('2023-05-13-run12.csv')
 
-# # 使用差分来找出mso_area_t变化的方向
-# diff = np.diff(df['mso_area_t'])
-# direction = np.sign(diff)
 
 # 使用差分来找出mso_area_t变化的方向
 diff_mso = np.diff(df['mso_area_t'])
@@ -20,9 +17,9 @@ diff_gmd1 = np.diff(df['gmd1_t'])
 gmd1_direction = np.sign(diff_gmd1)
 
 # 找出gmd1_t与mso_area_t变化趋势一致的数据点
-# mask = np.concatenate(([False], gmd1_direction == direction))
-mask = gmd1_direction == direction
-linear_df = df.iloc[:-1][mask]
+mask = np.concatenate(([True], gmd1_direction == direction))
+# mask = gmd1_direction == direction
+linear_df = df.iloc[:][mask]
 
 # # 找出gmd1_t与mso_area_t变化趋势一致的数据点
 # mask = np.concatenate(([False], np.diff(direction) != 0))
@@ -50,19 +47,25 @@ ax2 = fig.add_subplot(1, 2, 2)
 ax2.plot(linear_df['gmd1_timestamp_t'], linear_df['gmd1_t']*1000, label='gmd1_t',c='red')
 ax2.plot(linear_df['mso_timestamp_t'], linear_df['mso_area_t'], label='mso_area_t',c='blue')
 
+print('gmd1_t length:',len(linear_df['gmd1_t']))
+print('mso_area_t length:',len(linear_df['mso_area_t']))
+print('gmd1_timestamp_t length:',len(linear_df['gmd1_timestamp_t']))
+print('mso_timestamp_t length:',len(linear_df['mso_timestamp_t']))
+
 # 添加标签
 ax2.set_xlabel('timestamp')
 ax2.set_ylabel('value')
-ax2.set_title('GMD1 and MSO_AREA over time')
+# ax2.set_title('GMD1 and MSO_AREA over time')
 ax2.legend()
 
-print('gmd1_t length:',len(linear_df['gmd1_t']))
-print('mso_area_t length:',len(linear_df['mso_area_t']))
+# print('gmd1_t length:',len(linear_df['gmd1_t']))
+# print('mso_area_t length:',len(linear_df['mso_area_t']))
 
 # # 获取linear_df['gmd1_timestamp_t']的索引值
 indexes = linear_df['gmd1_t'].index
+
 # # 打印索引值
-# print(gmd1_t_indexes)
+print(indexes)
 
 
 
@@ -74,14 +77,59 @@ ax3.scatter(linear_df['gmd1_t'][:-1], linear_df['mso_area_t'][1:], label='gmd1_t
 ax4= fig2.add_subplot(1, 2, 2)
 ax4.plot(indexes,linear_df['mso_timestamp_t']-linear_df['gmd1_timestamp_t'], label='gmd1_t vs mso_area_t',c='red')
 
-x = indexes
-y = linear_df['mso_timestamp_t']
-coefficients = np.polyfit(x, y, 1)
-polyfit_fn = np.poly1d(coefficients)
-print(polyfit_fn)
-# 非线性部分
-fig3= plt.figure(figsize=(6, 4))
-ax5= fig3.add_subplot(1, 2, 1)
-ax5.plot(indexes,linear_df['mso_timestamp_t']-polyfit_fn(indexes), label='gmd1_t vs mso_area_t',c='red')
+
+# 剔除gmd1的时间戳中偏差很大的值
+gmd1_timestamp = linear_df['gmd1_timestamp_t']
+# 计算标准差
+diff = np.diff(gmd1_timestamp)
+std = diff.std()
+
+# 定义阈值（可以根据需要进行调整）
+threshold = 0.3
+
+# 根据阈值剔除偏差很大的值
+filter_index = np.concatenate(([True],np.abs(diff - diff.mean()) <= threshold * std))
+filtered_df = linear_df.iloc[:][filter_index]
+
+
+# print(len(filter_index))
+# print(len(filtered_df['gmd1_timestamp_t']))
+indexed2 = filtered_df['gmd1_timestamp_t'].index
+print('filter length:',len(indexed2))
+# 绘制符合要求的gmd1_t的时间戳数据点
+fig4 = plt.figure(figsize=(6, 4))
+ax6 = fig4.add_subplot(1, 2, 1)
+ax6.plot(indexed2,(filtered_df['mso_timestamp_t']-filtered_df['gmd1_timestamp_t']), label='gmd1_t',c='red')
+
+
+# # 同样的方法剔除mso_area的时间戳中偏差很大的值
+# mso_timestamp = filtered_df['mso_timestamp_t']
+# diff = np.diff(mso_timestamp)
+# std = diff.std()
+# filter_index2 = np.concatenate(([True],np.abs(diff - diff.mean()) <= threshold * std))
+# filtered_df2 = filtered_df.iloc[:][filter_index2]
+#
+# indexed3 = filtered_df2['mso_timestamp_t'].index
+# print('filter2 length:',len(indexed3))
+# # 绘制符合要求的mso_area_t的时间戳数据点
+# ax7 = fig4.add_subplot(1, 2, 2)
+# ax7.plot(indexed3,(filtered_df2['mso_timestamp_t']-filtered_df2['gmd1_timestamp_t']), label='mso_area_t',c='blue')
+
+
+
+
+
+
+# x = indexes
+# y = linear_df['mso_timestamp_t']
+#
+# coefficients = np.polyfit(x, y, 1)
+# polyfit_fn = np.poly1d(coefficients)
+# print(polyfit_fn)
+# # 非线性部分
+#
+# fig3= plt.figure(figsize=(6, 4))
+# ax5= fig3.add_subplot(1, 2, 1)
+# ax5.plot(indexes,linear_df['mso_timestamp_t']-polyfit_fn(indexes), label='gmd1_t vs mso_area_t',c='red')
 
 plt.show()
---
+++ test8.py
@@ -0,0 +1,203 @@
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+"""
+test8.py是test5.py数据处理的改进型
+"""
+plt.style.use("ggplot")
+
+number = 15
+filename = f'timestamp_test/2023-05-13/run{number}/data.hdf5'
+
+with h5py.File(filename, 'r') as f:
+    keys = list(f.keys())
+    if keys[0] == 'SBP:FE:GMD1_Ion:Curr:AI' and keys[2] == 'mso:Area':
+        gmd1_t = f[keys[0]][:]
+        gmd1_timestamp_t = f[keys[1]][:]
+        mso_area_t = f[keys[2]][:]
+        mso_timestamp_t = f[keys[3]][:]
+
+    else:
+        # 引发异常
+        raise ValueError('keys error')
+
+
+
+    # 寻找最佳 delta_t
+    max_delta_t = 0.3  # 最大时间差 0.3
+    best_delta_t = 0
+    max_corr_coef = 0
+    for delta_t in np.arange(-max_delta_t, max_delta_t + 0.001, 0.001):
+        mso_area_interp = np.interp(gmd1_timestamp_t + delta_t, mso_timestamp_t, mso_area_t)
+
+        corr_coef = np.corrcoef(gmd1_t, mso_area_interp)[0, 1]
+        if corr_coef > max_corr_coef:
+            max_corr_coef = corr_coef
+            best_delta_t = delta_t
+    print('best delta_t:', best_delta_t)
+    print('best corr_coef:', max_corr_coef)
+
+    # 找到了最佳的时间校准值
+    gmd1_timestamp_t = gmd1_timestamp_t + best_delta_t
+
+    # 将四个数据集合并为一个DataFrame
+    df = pd.DataFrame({
+        'gmd1_timestamp_t': gmd1_timestamp_t,
+        'gmd1_t': gmd1_t,
+        'mso_timestamp_t': mso_timestamp_t,
+        'mso_area_t': mso_area_t,
+    })
+
+    # 新建一个空的DataFrame
+    aligned_df = pd.DataFrame(columns=['gmd1_timestamp_t', 'gmd1_t', 'mso_timestamp_t', 'mso_area_t'])
+
+    # 记录前一个索引的row_shift
+    row_shift_pre = 1
+    delete_index = []
+    # 根据近似相等关系重新建立索引
+    for index, gmd1_time in enumerate(df['gmd1_timestamp_t']):
+        nearest_index = np.argmin(np.abs(df['mso_timestamp_t'] - gmd1_time))
+        row_shift = nearest_index - index
+
+
+        if row_shift == 1:
+            if row_shift_pre != row_shift:
+                if index - 1 > 0:
+                    delete_index.append(index - 1)
+                    print(delete_index)
+            # 将当前索引的gmd1_timestamp_t值保存到aligned_df
+            aligned_df.loc[index, 'gmd1_timestamp_t'] = df.loc[index, 'gmd1_timestamp_t']
+            # 将当前索引的gmd1_t值保存到aligned_df
+            aligned_df.loc[index, 'gmd1_t'] = df.loc[index, 'gmd1_t']
+            # 将下一个索引的mso_timestamp_t值保存到aligned_df
+            aligned_df.loc[index, 'mso_timestamp_t'] = df.loc[index + 1, 'mso_timestamp_t']
+            # 将下一个索引的mso_area_t值保存到aligned_df
+            aligned_df.loc[index, 'mso_area_t'] = df.loc[index + 1, 'mso_area_t']
+            row_shift_pre = row_shift
+        elif row_shift == 2:
+            if row_shift_pre != row_shift:
+                if index - 1 > 0:
+                    delete_index.append(index - 1)
+                    delete_index.append(index )
+                    print(delete_index)
+            # 将当前索引的gmd1_timestamp_t值保存到aligned_df
+            aligned_df.loc[index, 'gmd1_timestamp_t'] = df.loc[index, 'gmd1_timestamp_t']
+            # 将当前索引的gmd1_t值保存到aligned_df
+            aligned_df.loc[index, 'gmd1_t'] = df.loc[index, 'gmd1_t']
+            # 将下2个索引的mso_timestamp_t值保存到aligned_df
+            aligned_df.loc[index, 'mso_timestamp_t'] = df.loc[index + 2, 'mso_timestamp_t']
+            # 将下2个索引的mso_area_t值保存到aligned_df
+            aligned_df.loc[index, 'mso_area_t'] = df.loc[index + 2, 'mso_area_t']
+
+            row_shift_pre = row_shift
+
+        elif row_shift == 0:
+
+            if row_shift_pre != row_shift:
+                if index - 1 > 0:
+                    delete_index.append(index - 1)
+                    print(delete_index)
+
+            # 将当前索引的gmd1_timestamp_t值保存到aligned_df
+            aligned_df.loc[index, 'gmd1_timestamp_t'] = df.loc[index, 'gmd1_timestamp_t']
+            # 将当前索引的gmd1_t值保存到aligned_df
+            aligned_df.loc[index, 'gmd1_t'] = df.loc[index, 'gmd1_t']
+            # 将当前索引的mso_timestamp_t值保存到aligned_df
+            aligned_df.loc[index, 'mso_timestamp_t'] = df.loc[index, 'mso_timestamp_t']
+            # 将当前索引的mso_area_t值保存到aligned_df
+            aligned_df.loc[index, 'mso_area_t'] = df.loc[index, 'mso_area_t']
+
+            row_shift_pre = row_shift
+        elif row_shift == -1:
+            if row_shift_pre != row_shift:
+                if index - 1>0:
+                    delete_index.append(index-1)
+                    print(delete_index)
+
+
+            # 将当前索引的gmd1_timestamp_t值保存到aligned_df
+            aligned_df.loc[index, 'gmd1_timestamp_t'] = df.loc[index, 'gmd1_timestamp_t']
+            # 将当前索引的gmd1_t值保存到aligned_df
+            aligned_df.loc[index, 'gmd1_t'] = df.loc[index, 'gmd1_t']
+            # 将上一个索引的mso_timestamp_t值保存到aligned_df
+            aligned_df.loc[index, 'mso_timestamp_t'] = df.loc[index , 'mso_timestamp_t']
+            # 将上一个索引的mso_area_t值保存到aligned_df
+            aligned_df.loc[index, 'mso_area_t'] = df.loc[index , 'mso_area_t']
+
+            row_shift_pre = row_shift
+        print(f'Row shift for gmd1_timestamp_t at index {index}: {row_shift}')
+
+    # 删除第一行
+    # df.dropna(inplace=True)
+    # 重设索引
+    # df.reset_index(drop=True, inplace=True)
+    # 保存为csv文件,显示索引
+    # df.to_csv('data2.csv', index=True)
+
+    # # 重设索引
+    # # aligned_df.reset_index(drop=True, inplace=True)
+
+    aligned_df.drop(index= delete_index,inplace=True)
+
+    # idx = np.where(abs(aligned_df['gmd1_timestamp_t'] - aligned_df['mso_timestamp_t']) > 0.15)
+    # print(idx)
+
+    # 找到相邻的mso_timestamp_t差值大于10的索引号
+    idx = np.where(abs(np.diff(aligned_df['mso_timestamp_t'])) > 0.5)
+    print(idx)
+    # 打印结果
+    print(aligned_df.iloc[idx])
+
+    aligned_df.to_csv(f'aligned_data{number}.csv', index=True)
+
+
+    # 绘制符合要求的gmd1_t数据点
+    fig = plt.figure(figsize=(6, 4))
+    fig.suptitle(filename)
+    ax = fig.add_subplot(1, 4, 1)
+    # ax.plot(aligned_df['gmd1_timestamp_t'], aligned_df['gmd1_t'], label='(new)gmd1_t', c='red')
+    ax.scatter(aligned_df['gmd1_t'], aligned_df['mso_area_t'], label='gmd1_t vs mso_area_t', c='red')
+    # 添加标签
+    # ax.set_xlabel('timestamp')
+    # ax.set_ylabel('value')
+    # ax.set_title('GMD1 and MSO_AREA over time')
+    ax.legend()
+
+
+    ax4 = fig.add_subplot(1, 4, 2)
+    ax4.plot(np.diff(aligned_df['gmd1_timestamp_t']), label="gmd.timestamp")
+    ax4.plot(np.diff(aligned_df['mso_timestamp_t']), label= "pd.timestamp")
+    ax4.set_xlabel("pulse number")
+    ax4.set_ylabel("interval")
+    ax4.set_title("interval of timestamp")
+    ax4.legend()
+
+    ax5 = fig.add_subplot(1, 4, 3)
+    ax5.plot( aligned_df['mso_timestamp_t']-aligned_df['gmd1_timestamp_t'] , label="timestamp difference")
+    ax5.set_xlabel("pulse number")
+    ax5.set_ylabel("difference")
+    ax5.set_title("difference between\n %s \n and %s" % ("pd", "gmd"))
+    ax5.legend()
+
+    index1 = aligned_df['gmd1_timestamp_t'].index
+    index2 = aligned_df['mso_timestamp_t'].index
+    # print(index1)
+    # print(range(len(index1)))
+    p1 = np.polyfit(index1, aligned_df['gmd1_timestamp_t'].astype(float), 1)
+    p2 = np.polyfit(index2, aligned_df['mso_timestamp_t'].astype(float), 1)
+    linear_part1 = np.polyval(p1, index1)
+    nonlinear_part1 = aligned_df['gmd1_timestamp_t'] - linear_part1
+    linear_part2 = np.polyval(p2, index2)
+    nonlinear_part2 = aligned_df['mso_timestamp_t'] - linear_part2
+
+    ax6 = fig.add_subplot(1, 4, 4)
+    ax6.plot( nonlinear_part1,label=  "gmd.timestamp")
+    ax6.plot(nonlinear_part2, label= "pd.timestamp")
+
+    ax6.set_xlabel("pulse number")
+    ax6.set_ylabel("nonlinear")
+    ax6.set_title("nonlinear of timestamp")
+    ax6.legend()
+
+    plt.show()
